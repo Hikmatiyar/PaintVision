9bcf1a4107cfd090020d65fbde8bfe44d8fc1ff7
Hikmatiyar <Hikmatiyarm@gmail.com>
2024-12-17 15:01:30 -0600

    Update login.module.ts

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule
+  ],
+  declarations: [LoginComponent]
 })
-export class LoginModule {}
+export class LoginPageModule {}
